9b940fffc6ea5382c02c890059ce2a690d38d179
Greg Landry <gjl@cypress.com>
2016-10-14 20:04:00 -0400

    Rename chapter 02 files from 01. Add i2c scan project. Add exercise to update platform files. Remove magic numbers.

--- projects/WICED/wa101key/02/12_i2cread/12_i2cread.c
+++ projects/WICED/wa101key/02/12_i2cread/12_i2cread.c
@@ -1,5 +1,9 @@
 #include "wiced.h"
 
+#define I2C_ADDRESS (0x08)
+#define RETRIES (1)
+#define DISABLE_DMA (0)
+#define NUM_MESSAGES (1)
 
 volatile wiced_bool_t buttonPress = WICED_FALSE;
 
@@ -19,7 +23,7 @@ void application_start( )
     /* Setup I2C master */
     const wiced_i2c_device_t i2cDevice = {
     	.port = WICED_I2C_1,
-		.address = 0x08,
+		.address = I2C_ADDRESS,
 		.address_width = I2C_ADDRESS_WIDTH_7BIT,
 		.speed_mode = I2C_STANDARD_SPEED_MODE
     };
@@ -29,22 +33,22 @@ void application_start( )
     /* Tx buffer is used to set the offset */
     uint8_t tx_buffer[] = {2};
     wiced_i2c_message_t setOffset;
-    wiced_i2c_init_tx_message(&setOffset, tx_buffer, sizeof(tx_buffer), 1, 0);
+    wiced_i2c_init_tx_message(&setOffset, tx_buffer, sizeof(tx_buffer), RETRIES, DISABLE_DMA);
 
     /* Rx buffer is used to get temperature and humidity data */
     uint16_t rx_buffer[] = {0,0};
     wiced_i2c_message_t msg;
-    wiced_i2c_init_rx_message(&msg, rx_buffer, sizeof(rx_buffer), 1, 0);
+    wiced_i2c_init_rx_message(&msg, rx_buffer, sizeof(rx_buffer), RETRIES, DISABLE_DMA);
 
     /* Initialize offset */
-    wiced_i2c_transfer(&i2cDevice, &setOffset, 1);
+    wiced_i2c_transfer(&i2cDevice, &setOffset, NUM_MESSAGES);
 
 
     while ( 1 )
     {
     	if(buttonPress)
     	{
-    		wiced_i2c_transfer(&i2cDevice, &msg, 1); /* Get new data from I2C */
+    		wiced_i2c_transfer(&i2cDevice, &msg, NUM_MESSAGES); /* Get new data from I2C */
     		WPRINT_APP_INFO(("Temperature: %d\tHumidity: %d\n", rx_buffer[0], rx_buffer[1])); /* Print data to terminal */
 
     		buttonPress = WICED_FALSE; /* Reset flag for next button press */
