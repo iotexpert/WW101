04e07b81d2bcbe08e9dfd7ac52a51a78cf426eb2
ahawse <arh@cypress.com>
2016-11-02 12:46:28 -0400

    updated for chapter 6 and projects

---
+++ projects/WICED/wa101key/06/01_client/01_client.mk
@@ -0,0 +1,15 @@
+#
+# Broadcom Proprietary and Confidential. Copyright 2016 Broadcom
+# All Rights Reserved.
+#
+# This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
+# the contents of this file may not be disclosed to third parties, copied
+# or duplicated in any form, in whole or in part, without the prior
+# written permission of Broadcom Corporation.
+#
+
+NAME := App_tcp_client
+
+$(NAME)_SOURCES := 01_client.c
+
+WIFI_CONFIG_DCT_H := wifi_config_dct.h
