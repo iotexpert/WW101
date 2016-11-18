83cde93a529ab73c70d10b3b3b44eeeacd678b6a
ahawse <arh@cypress.com>
2016-11-18 12:33:11 -0500

    arh updates

---
+++ projects/WICED/wa101key/05/01_attach_open/01_attach_open.mk
@@ -0,0 +1,15 @@
+#
+# Broadcom Proprietary and Confidential. Copyright 2016 Broadcom
+# All Rights Reserved.
+#
+# This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
+# the contents of this file may not be disclosed to third parties, copied
+# or duplicated in any form, in whole or in part, without the prior
+# written permission of Broadcom Corporation.
+#
+
+NAME := App_01_Attach
+
+$(NAME)_SOURCES := 01_attach_open.c
+
+WIFI_CONFIG_DCT_H := wifi_config_dct.h
