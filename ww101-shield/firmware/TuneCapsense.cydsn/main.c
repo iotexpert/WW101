6e7f9a613579d318069b71ba414074da99455a30
Greg Landry <gjl@cypress.com>
2017-03-03 16:35:50 -0500

    Update CapSense tuning. Add LEDs to the CapSense tuning project including LED1 for Proximity.

--- ww101-shield/firmware/TuneCapsense.cydsn/main.c
+++ ww101-shield/firmware/TuneCapsense.cydsn/main.c
@@ -29,6 +29,38 @@ int main(void)
         if(!CapSense_IsBusy())
         {
             CapSense_ProcessAllWidgets();
+            
+            if(CapSense_IsWidgetActive(CapSense_BUTTON0_WDGT_ID))
+            {
+                CSLED0_Write(0);
+            } else {
+                CSLED0_Write(1);
+            }
+            if(CapSense_IsWidgetActive(CapSense_BUTTON1_WDGT_ID))
+            {
+                CSLED1_Write(0);
+            } else {
+                CSLED1_Write(1);
+            }
+            if(CapSense_IsWidgetActive(CapSense_BUTTON2_WDGT_ID))
+            {
+                CSLED2_Write(0);
+            } else {
+                CSLED2_Write(1);
+            }
+            if(CapSense_IsWidgetActive(CapSense_BUTTON3_WDGT_ID))
+            {
+                CSLED3_Write(0);
+            } else {
+                CSLED3_Write(1);
+            }
+            if(CapSense_IsWidgetActive(CapSense_PROXIMITY0_WDGT_ID))
+            {
+                ProxLED_Write(0);
+            } else {
+                ProxLED_Write(1);
+            }
+            
             CapSense_RunTuner();
             CapSense_ScanAllWidgets();
         }
