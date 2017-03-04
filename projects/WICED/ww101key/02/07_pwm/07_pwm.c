343e20b05f073ff6245ba98d906b2a527e69fe2d
Mike Noel <mno@cypress.com>
2017-03-04 06:07:07 -0800

    added Azure tools bins for simulating a device, reading a device, and creating a client id. Also updated the 07_pwm solution

--- projects/WICED/ww101key/02/07_pwm/07_pwm.c
+++ projects/WICED/ww101key/02/07_pwm/07_pwm.c
@@ -3,21 +3,24 @@
 
 void application_start( )
 {
-	float duty_cycle = 50.0;
+    float duty_cycle = 50.0;
+    float cutoff = 100.0;
+    uint32_t loopDelay = 10;
+    uint32_t frequency = 500;
 
     wiced_init();	/* Initialize the WICED device */
 
-    wiced_pwm_init(WICED_PWM_4, 500, duty_cycle);
+    wiced_pwm_init(WICED_PWM_4, frequency, duty_cycle);
     wiced_pwm_start(WICED_PWM_4);
 
     while ( 1 )
     {
-        wiced_pwm_init(WICED_PWM_4, 500, duty_cycle);
+        wiced_pwm_init(WICED_PWM_4, frequency, duty_cycle);
         duty_cycle += 1.0;
-        if(duty_cycle > 100.0)
+        if(duty_cycle > cutoff)
         {
         	duty_cycle = 0.0;
         }
-        wiced_rtos_delay_milliseconds( 10 );
+        wiced_rtos_delay_milliseconds(loopDelay);
     }
 }
