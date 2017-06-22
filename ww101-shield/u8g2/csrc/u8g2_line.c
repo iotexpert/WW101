2c4a9a6d7170989a0af6024cb59959470ee7d1da
Greg Landry <gjl@cypress.com>
2017-06-22 17:27:06 -0400

    Update CapSense buttons to manual tuning but Humidity to auto-tune.

--- ww101-shield/u8g2/csrc/u8g2_line.c
+++ ww101-shield/u8g2/csrc/u8g2_line.c
@@ -1,22 +1,16 @@
 /*
-
   u8g2_box.c
-
   Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
-
   Copyright (c) 2016, olikraus@gmail.com
   All rights reserved.
-
   Redistribution and use in source and binary forms, with or without modification, 
   are permitted provided that the following conditions are met:
-
   * Redistributions of source code must retain the above copyright notice, this list 
     of conditions and the following disclaimer.
     
   * Redistributions in binary form must reproduce the above copyright notice, this 
     list of conditions and the following disclaimer in the documentation and/or other 
     materials provided with the distribution.
-
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
@@ -30,7 +24,6 @@
   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-
 */
 
 #include "u8g2.h"
@@ -89,4 +82,3 @@ void u8g2_DrawLine(u8g2_t *u8g2, u8g2_uint_t x1, u8g2_uint_t y1, u8g2_uint_t x2,
     }
   }
 }
-
